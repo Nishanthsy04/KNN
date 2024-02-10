1dfa3a87b7d82c6a71c0f7c4b561fbaadd989403
Nishanthsy04 <121054032+Nishanthsy04@users.noreply.github.com>
2024-02-10 22:59:52 +0530

    Update KNN.py

--- KNN.py
+++ KNN.py
@@ -3,7 +3,7 @@ from sklearn.neighbors import KNeighborsClassifier
 from sklearn.datasets import load_iris
 from sklearn.metrics import accuracy_score
 from sklearn.model_selection import train_test_split
-data=pd.read_csv("knn_iris.csv")
+data=pd.read_csv("iris.csv")
 x=data.iloc[:,:-1].values
 y=data.iloc[:,-1].values
 x_train,x_test,y_train,y_test=train_test_split(x,y,test_size=0.3,random_state=5
